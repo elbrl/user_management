4d2e7b34d71b77ecbed76ce355452e03a0157b23
Elberel Batbayar <elberelbatbayar@Elberels-MacBook-Pro.local>
2024-02-07 17:09:51 +0800

    dashboard,users page got data with mongodb

--- app/component/ListUser.tsx
+++ app/component/ListUser.tsx
@@ -2,7 +2,31 @@ import Image from "next/image";
 import { users } from "./UsersData";
 import Footer from "./Footer";
 
-export default function ListUser() {
+interface mapType {
+  name: String;
+  _id: String;
+  role: String;
+  createdAt: String;
+  status: String;
+  mail: String;
+}
+const getUsers = async () => {
+  try {
+    const res = await fetch("http://localhost:3000/api/users", {
+      cache: "no-store",
+    });
+    if (!res.ok) {
+      throw new Error("Faild to fetch users");
+    }
+    return res.json();
+  } catch (erorr) {
+    console.log("Error loading users: ", erorr);
+  }
+};
+
+export default async function ListUser() {
+  const { users } = await getUsers();
+  const limitedUsers = users.slice(0, 8);
   return (
     <div className="mx-8 text-black h-[73.6vh]">
       <div className=" h-[65vh] mt-8 rounded-2xl bg-white w-full ">
@@ -22,7 +46,7 @@ export default function ListUser() {
           </div>
         </div>
 
-        {users.map(({ name, mail, status, date, role }) => (
+        {limitedUsers.map((t: mapType) => (
           <div>
             <div className="flex justify-center">
               <div className="h-[1px] w-full bg-slate-100 mx-4"></div>
@@ -30,29 +54,29 @@ export default function ListUser() {
             <div className="flex flex-row">
               <div className="2xl:h-[70px] tg:h-[64px] flex items-center w-[50vw] justify-between">
                 <div className="m-6 ml-[70px] w-40">
-                  <div className="tg:text-xs md:text-sm">{name}</div>
+                  <div className="tg:text-xs md:text-sm"> {t.name}</div>
                   <div className="tg:text-[10px] md:text-xs text-gray-400 ">
-                    {mail}
+                    {t.mail}
                   </div>
                 </div>
                 <div className="tg:hidden 2lg:inline">
                   <div
                     className={`w-28 mr-[70px] ${
-                      status === "Employee"
+                      t.status === "Employee"
                         ? "bg-slate-100 text-slate-400"
                         : "bg-[#0095FF] text-white"
                     } h-7 rounded-lg  flex items-center justify-center`}
                   >
-                    <div className="tg:text-xs md:text-sm">{status}</div>
+                    <div className="tg:text-xs md:text-sm">{t.status}</div>
                   </div>
                 </div>
               </div>
               <div className="h-16 flex items-center w-[50vw] justify-between md:mx-4">
                 <div className="text-gray-600 tg:text-[10px] md:text-xs">
-                  {date}
+                  {t.createdAt.slice(0, 10)}
                 </div>
                 <div className="tg:hidden 2lg:inline text-gray-600 tg:text-[10px] md:text-xs">
-                  {role}
+                  {t.role}
                 </div>
                 <div className="flex flex-row">
                   <Image
--- app/component/ListUser2.tsx
+++ app/component/ListUser2.tsx
@@ -1,79 +1,12 @@
-// import Image from "next/image";
-// import { users2 } from "./UsersData";
-// import Footer from "./Footer";
-
-// export default function ListUser2() {
-//   return (
-//     <div className="mx-8 text-black h-80">
-//       <div className="rounded-lg bg-white">
-//         <div className="rounded-t-lg bg-[#EFF4FA] h-16 flex justify-around items-center">
-//           <div className="text-slate-400 w-[45%] flex flex-row items-center justify-between tg:text-sm md:text-base">
-//             <div className="w-[30%]">Name</div>
-//             <div className="tg:w-16 lg:w-2/12 tg:mr-10 lg:mr-0">User ID</div>
-//             <div className="w-[120px] tg:hidden lg:inline ">Lorem ipsum</div>
-//           </div>
-//           <div className="text-slate-400 w-[45%] flex flex-row items-center justify-between">
-//             <div className="w-4/12 tg:ml-6 lg:ml-0">Date</div>
-//             <div className="w-[120px] tg:hidden lg:inline ">Lorem ipsum</div>
-//             <div className="w-[15%]">Action</div>
-//           </div>
-//         </div>
-//         {users2.map(({ name, userId, date, role }) => (
-//           <div>
-//             <div className="h-16 flex justify-around items-center font-light tg:text-xs md:text-sm">
-//               <div className="w-[45%] flex flex-row justify-between">
-//                 <div className="w-3/12 ml-8 font-normal tg:text-xs md:text-sm">
-//                   {name}
-//                 </div>
-//                 <div className="tg:w-16 lg:w-2/12 tg:text-[10px] md:text-xs tg:mr-10 lg:mr-0">
-//                   {userId}
-//                 </div>
-//                 <div className="w-[120px] tg:hidden lg:inline tg:text-[10px] md:text-xs ">
-//                   {role}
-//                 </div>
-//               </div>
-//               <div className="w-[45%] flex flex-row items-center justify-between ">
-//                 <div className="tg:w-60 lg:w-4/12 tg:text-[10px] md:text-xs tg:ml-6 lg:ml-0">
-//                   {date}
-//                 </div>
-//                 <div className="w-[100px] tg:hidden lg:inline tg:text-[10px] md:text-xs ">
-//                   {role}
-//                 </div>
-//                 <div className="tg:w-16 lg:w-2/12 tg:mr-6 lg:mr-0">
-//                   <div className="flex flex-row">
-//                     <Image
-//                       className="m-3"
-//                       src={"/check.svg"}
-//                       width={25}
-//                       height={25}
-//                       alt="Check"
-//                     />
-//                     <Image
-//                       className="m-3"
-//                       src={"/trash.svg"}
-//                       width={25}
-//                       height={25}
-//                       alt="Trash"
-//                     />
-//                   </div>
-//                 </div>
-//               </div>
-//             </div>
-//             <div className="h-[1px] w-full bg-slate-100 mx-4"></div>
-//           </div>
-//         ))}
-//       </div>
-//       <div className="tg:h-[350px] sm:h-[222px] flex items-end justify-center ">
-//         <div className="mb-4">
-//           <Footer />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
 import Image from "next/image";
 import Footer from "./Footer";
+
+interface mapType {
+  name: String;
+  _id: String;
+  role: String;
+  createdAt: String;
+}
 
 const getUsers = async () => {
   try {
@@ -91,7 +24,7 @@ const getUsers = async () => {
 
 export default async function ListUser2() {
   const { users } = await getUsers();
-
+  const limitedUsers = users.slice(0, 4);
   return (
     <div className="mx-8 text-black h-80 ">
       <div className="rounded-lg bg-white">
@@ -107,7 +40,7 @@ export default async function ListUser2() {
             <div className="w-[15%]">Action</div>
           </div>
         </div>
-        {users.map((t: String) => (
+        {limitedUsers.map((t: mapType) => (
           <div>
             <div className="h-16 flex justify-around items-center font-light tg:text-xs md:text-sm">
               <div className="w-[45%] flex flex-row justify-between">
@@ -115,7 +48,7 @@ export default async function ListUser2() {
                   {t.name}
                 </div>
                 <div className="tg:w-16 lg:w-2/12 tg:text-[10px] md:text-xs tg:mr-10 lg:mr-0">
-                  {/* {userId} */}
+                  {t._id.slice(-6)}
                 </div>
                 <div className="w-[120px] tg:hidden lg:inline tg:text-[10px] md:text-xs ">
                   {t.role}
@@ -123,10 +56,10 @@ export default async function ListUser2() {
               </div>
               <div className="w-[45%] flex flex-row items-center justify-between ">
                 <div className="tg:w-60 lg:w-4/12 tg:text-[10px] md:text-xs tg:ml-6 lg:ml-0">
-                  {/* {date} */}
+                  {t.createdAt.slice(0, 10)}
                 </div>
                 <div className="w-[100px] tg:hidden lg:inline tg:text-[10px] md:text-xs ">
-                  {/* {role} */}
+                  {t.role}
                 </div>
                 <div className="tg:w-16 lg:w-2/12 tg:mr-6 lg:mr-0">
                   <div className="flex flex-row">
